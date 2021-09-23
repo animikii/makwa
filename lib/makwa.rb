61d57c7f32948891761197158d631bfdddcc7898
Jo Hund <jhund@clearcove.ca>
2021-09-23 08:44:11 -0700

    Initial commit

---
+++ lib/makwa.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "makwa/version"
+
+module Makwa
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/makwa/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Makwa
+  VERSION = "0.1.0"
+end
